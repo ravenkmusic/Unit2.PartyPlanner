3ea80e258715a5a544ccc643b4dc72ec134ebfd8
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-22 16:40:46 -0500

    added API and state

---
+++ index.js
@@ -0,0 +1,10 @@
+const COHORT = "2308-acc-et-web-pt-b";
+const endPoint = "/events";
+const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT + endPoint;
+
+const state = {
+  events: [],
+  event: null,
+  guests: [],
+  rsvps: [],
+};
