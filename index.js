8d3ad034101f734bb6269d7d4acaf0f462cdd0a6
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-26 14:36:56 -0500

    edited API variable for readability

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const COHORT = "2308-acc-et-web-pt-b";
 const endPoint = "/events";
-const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT + endPoint;
+const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/${endPoint}`;
 
 const state = {
   events: [],
