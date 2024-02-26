e848ca09117d8180d1b78286c2d6ac8dcc9b9725
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-26 16:23:40 -0500

    created function to add parties and removed typo in API variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const COHORT = "2308-acc-et-web-pt-b";
 const endPoint = "/events";
-const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/${endPoint}`;
+const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}${endPoint}`;
 
 const state = {
   parties: [],
@@ -9,9 +9,13 @@ const state = {
 //references
 const partyList = document.querySelector("#parties");
 const addPartyForm = document.querySelector("#addParty");
+const deletePartyButton = document.querySelector("#deleteParty");
 
 //event listener for adding parties
 addPartyForm.addEventListener("submit", addParty);
+
+//event listener for deleting parties
+//deletePartyButton.addEventListener("click", deleteParty);
 
 //function that synchronizes state and displays all events including user's added events
 async function displayAllParties(){
@@ -42,7 +46,7 @@ function getParties () {
   const partyListing = state.parties.map((party) => {
 
     const li = document.createElement("li");
-    const partyName = party.name.toUpperCase();
+    const partyName = party.name;
     const partyDescription = party.description;
     const partyDate = party.date;
     const partyLocation = party.location;
@@ -54,6 +58,31 @@ function getParties () {
     <p><button id="deleteParty">Delete</button></p>`;
     return li;
   });
-
+  //refresh list
   partyList.replaceChildren(...partyListing);
+}
+
+//add party to api
+async function addParty(event){
+  event.preventDefault();
+
+  try {
+    const response = await fetch(API, {
+      method: "POST",
+      headers: { "Content-Type" : "application/json" },
+      body: JSON.stringify({
+        name: addPartyForm.name.value,
+        Date: addPartyForm.date.value,
+        description: addPartyForm.description.value,
+      }),
+    });
+
+    if(!response.ok) {
+      throw new Error("Cannot create party, try again.");
+    }
+
+    displayAllParties();
+  } catch (error) {
+    console.error(error);   
+  }
 }
